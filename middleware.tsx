6677de7d19e31e45477fb69fca68e8e5ae89ae00
dato korgalidze <d.korgalidze@gmail.com>
2024-03-06 16:03:33 +0400

    add public routes in middleware

--- middleware.tsx
+++ middleware.tsx
@@ -3,9 +3,16 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
  
   publicRoutes: [
-    '/',
-    '/ask-question'
-  ]
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
  
 export const config = {
